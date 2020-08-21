6a530acc9e333c02788a405e081f94b16787654a
Kevin Samosir <bintangkevin29@gmail.com>
2020-08-21 19:37:18 +0700

    added login route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.scss";
 import LoginPage from "./pages/LoginPages";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <LoginPage />
+      <Route exact path="/login">
+        <LoginPage />
+      </Route>
     </div>
   );
 }
