f9d4fb69edcbe97dce412fe7fc3d7bca3066f3d7
Kevin Samosir <bintangkevin29@gmail.com>
2020-08-22 19:08:40 +0700

    added example table

--- src/components/CustomTable/CustomTable.component.tsx
+++ src/components/CustomTable/CustomTable.component.tsx
@@ -2,18 +2,38 @@ import React from "react";
 
 import "./CustomTable.style.scss";
 
-const CustomTable: React.FC = () => {
+interface TableNode {
+  columns: {
+    header: string;
+    key: string;
+  }[];
+  data: object[];
+}
+
+interface Props {
+  tableData: TableNode;
+}
+
+const CustomTable: React.FC<Props> = ({ tableData }) => {
   return (
     <div className="customTable">
       <table className="table">
         <thead>
           <tr>
-            <th>Test</th>
-            <th>asd</th>
-            <th>as</th>
+            {tableData.columns.map((column) => (
+              <th key={column.key}>{column.header}</th>
+            ))}
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>
+          {tableData.data.map((dt, i) => (
+            <tr key={i}>
+              {tableData.columns.map((column, j) => (
+                <td key={j}>{dt[column.key]}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
--- src/pages/FinanceAcountPage/FinanceAcountPage.component.tsx
+++ src/pages/FinanceAcountPage/FinanceAcountPage.component.tsx
@@ -7,13 +7,51 @@ import CustomButton from "../../components/CustomButton";
 import CustomTable from "../../components/CustomTable";
 
 const FinanceAcountPage: React.FC = () => {
+  const tableData = {
+    columns: [
+      {
+        header: "Name",
+        key: "name",
+      },
+      {
+        header: "Description",
+        key: "desc",
+      },
+      {
+        header: "Type",
+        key: "type",
+      },
+      {
+        header: "Last Modified",
+        key: "last_modified",
+      },
+      {
+        header: "Created At",
+        key: "created_at",
+      },
+      {
+        header: "Deleted At",
+        key: "deleted_at",
+      },
+    ],
+    data: [
+      {
+        name: "Kevin",
+        desc: "Kevin",
+        type: "Kevin",
+        last_modified: "Kevin",
+        created_at: "Kevin",
+        deleted_at: "Kevin",
+      },
+    ],
+  };
   return (
     <div className="financeAccountPage">
       <div className="financeAccountPage__subHeader">
         <SearchInput className="financeAccountPage__search" />
         <CustomButton>Create New Account</CustomButton>
       </div>
-      <CustomTable />
+      <CustomTable tableData={tableData} />
     </div>
   );
 };
