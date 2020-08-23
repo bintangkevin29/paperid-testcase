e807315749930804d0a98999c7fcf943c506d38a
Kevin Samosir <bintangkevin29@gmail.com>
2020-08-23 10:28:26 +0700

    temporarily disable auto redirect on no token

--- src/components/ProtectedRoute/ProtectedRoute.component.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.component.tsx
@@ -1,7 +1,8 @@
 import React, { ReactNode } from "react";
-import { Route, Redirect } from "react-router-dom";
-import { RootState } from "../../redux/root.reducer";
-import { useSelector } from "react-redux";
+import { Route } from "react-router-dom";
+// import { Route, Redirect } from "react-router-dom";
+// import { RootState } from "../../redux/root.reducer";
+// import { useSelector } from "react-redux";
 
 interface Props {
   exact?: boolean;
@@ -10,10 +11,11 @@ interface Props {
 }
 
 const ProtectedRoute: React.FC<Props> = ({ exact = false, path, children }) => {
-  const token = useSelector((state: RootState) => state.auth.token);
+  // const token = useSelector((state: RootState) => state.auth.token);
   return (
     <Route exact={exact} path={path}>
-      {token ? children : <Redirect to="/login" />}
+      {/* {token ? children : <Redirect to="/login" />} */}
+      {children}
     </Route>
   );
 };
