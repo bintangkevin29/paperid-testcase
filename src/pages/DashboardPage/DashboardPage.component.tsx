d60d2988b910200b061bc3ddc9dd167d1df65490
Kevin Samosir <bintangkevin29@gmail.com>
2020-08-23 22:32:37 +0700

    modified dashboard style

--- src/pages/DashboardPage/DashboardPage.component.tsx
+++ src/pages/DashboardPage/DashboardPage.component.tsx
@@ -3,14 +3,22 @@ import { Helmet } from "react-helmet";
 
 import "./DashboardPage.style.scss";
 import CustomChart from "../../components/CustomChart";
+import { Row, Col } from "react-bootstrap";
 
 const DashboardPage: React.FC = () => {
   return (
-    <div>
+    <div className="dashboardPage">
       <Helmet>
         <title>Dashboard</title>
       </Helmet>
-      <CustomChart />
+      <Row>
+        <Col className="dashboardPage__transactionSummary" xs={12} md={6}>
+          <span className="dashboardPage__cardHeading">Transaction Summary</span>
+          <div className="dashboardPage__chartContainer">
+            <CustomChart />
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 };
