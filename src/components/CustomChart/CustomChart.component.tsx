67afb1df774e24d9a9d79d8833975b77d75d7e0e
Kevin Samosir <bintangkevin29@gmail.com>
2020-08-23 21:22:39 +0700

    added custom chart

---
+++ src/components/CustomChart/CustomChart.component.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./CustomChart.style.scss";
+
+const CustomChart: React.FC = () => {
+  return <div className="customChart"></div>;
+};
+
+export default CustomChart;
