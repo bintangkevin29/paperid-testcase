92e8c2899af99b5cdcaece31fcfbb8fc46cdcdcc
Kevin Samosir <bintangkevin29@gmail.com>
2020-08-23 23:31:10 +0700

    Updates

---
+++ precache-manifest.c48890b714c3930c24d940775286c876.js
@@ -0,0 +1,266 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8962c0510e89fc0f4722ca2e4e3f83e1",
+    "url": "/paperid-testcase/index.html"
+  },
+  {
+    "revision": "1be30593766b87f75cd2",
+    "url": "/paperid-testcase/static/css/main.5227187d.chunk.css"
+  },
+  {
+    "revision": "3c4192238e04e78d0add",
+    "url": "/paperid-testcase/static/js/2.21e1d832.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/paperid-testcase/static/js/2.21e1d832.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1be30593766b87f75cd2",
+    "url": "/paperid-testcase/static/js/main.07089396.chunk.js"
+  },
+  {
+    "revision": "422eabe9a9c325ecc34f",
+    "url": "/paperid-testcase/static/js/runtime-main.8c9b5fd5.js"
+  },
+  {
+    "revision": "91f854c7f51d4a8d7bc67991efec5e85",
+    "url": "/paperid-testcase/static/media/Gambar-Untuk-press-Release-02.91f854c7.png"
+  },
+  {
+    "revision": "5a55f095466725310d4f0a02f5071e4a",
+    "url": "/paperid-testcase/static/media/Gambar-Untuk-press-Release-02@2x.5a55f095.png"
+  },
+  {
+    "revision": "4b15b45952ea74b6cdfd2c86cf149ad8",
+    "url": "/paperid-testcase/static/media/Tutorial.4b15b459.svg"
+  },
+  {
+    "revision": "1919cb699214ecab600e276bc6b4692e",
+    "url": "/paperid-testcase/static/media/dashboard.1919cb69.svg"
+  },
+  {
+    "revision": "9c1a3210bfa8950f4550d187a0ded675",
+    "url": "/paperid-testcase/static/media/finance.9c1a3210.svg"
+  },
+  {
+    "revision": "7e1ea4fa16b2d5e118b13d3bc2d3c0a0",
+    "url": "/paperid-testcase/static/media/icon_account_white.7e1ea4fa.svg"
+  },
+  {
+    "revision": "fed42d919921dd8c7023f217be6cd664",
+    "url": "/paperid-testcase/static/media/icon_lock_white.fed42d91.svg"
+  },
+  {
+    "revision": "47e27cbed0532f3c7f1b2332490f979f",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-100.47e27cbe.eot"
+  },
+  {
+    "revision": "4dfdd1c035c641accc4f3fedb7b73055",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-100.4dfdd1c0.woff"
+  },
+  {
+    "revision": "9ca14b8703044ec44627982d6d0e1e44",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-100.9ca14b87.woff2"
+  },
+  {
+    "revision": "d9cec969ebe31edeb2b203f2db2f5205",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-100.d9cec969.ttf"
+  },
+  {
+    "revision": "fbba7ad644ab81c892f79b81e1267a8d",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-100.fbba7ad6.svg"
+  },
+  {
+    "revision": "83a45512c894889710e9966b9edddc6d",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-100italic.83a45512.woff"
+  },
+  {
+    "revision": "8c9d6d04bf0c001e2abae087594b007c",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-100italic.8c9d6d04.svg"
+  },
+  {
+    "revision": "93047059442e732088b45e417e2e495d",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-100italic.93047059.eot"
+  },
+  {
+    "revision": "bdc564929ce6358365d61401794f7fdd",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-100italic.bdc56492.ttf"
+  },
+  {
+    "revision": "de3428c265cecf1856805f21f274bb82",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-100italic.de3428c2.woff2"
+  },
+  {
+    "revision": "1b8f8afa9b4ba60fbcf9936eae918ec6",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-300.1b8f8afa.eot"
+  },
+  {
+    "revision": "483a27c988018f92133995b7d1453248",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-300.483a27c9.ttf"
+  },
+  {
+    "revision": "54d3cfe0cec734925a21cebfe378a12e",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-300.54d3cfe0.svg"
+  },
+  {
+    "revision": "656963c4beb814e754e38478fcaa0439",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-300.656963c4.woff"
+  },
+  {
+    "revision": "98d8cf792834c0bef59c2be99dc3533d",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-300.98d8cf79.woff2"
+  },
+  {
+    "revision": "17e346950dce164549968b7b93d48f2d",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-300italic.17e34695.svg"
+  },
+  {
+    "revision": "329d60785944501134891f948f41c001",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-300italic.329d6078.ttf"
+  },
+  {
+    "revision": "8e90b967ea69fc68b130d36cc34d41c0",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-300italic.8e90b967.woff"
+  },
+  {
+    "revision": "a21767e20d27a9c06007c981a8e5f827",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-300italic.a21767e2.woff2"
+  },
+  {
+    "revision": "f0024105229082db42a2c44171237cae",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-300italic.f0024105.eot"
+  },
+  {
+    "revision": "1efbd38aa76ddae2580fedf378276333",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-700.1efbd38a.woff2"
+  },
+  {
+    "revision": "596223f7bff9598c852e949e9d2b1aa0",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-700.596223f7.svg"
+  },
+  {
+    "revision": "874b8e7bc7e8d1507b50f56bc6c9b536",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-700.874b8e7b.woff"
+  },
+  {
+    "revision": "f9203469f750b4284c62aa49f9496cb0",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-700.f9203469.eot"
+  },
+  {
+    "revision": "fe7d53e4cac8f5b946a9e25f004efc8c",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-700.fe7d53e4.ttf"
+  },
+  {
+    "revision": "2121d6d839bb564bfcb3436cf0479404",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-700italic.2121d6d8.eot"
+  },
+  {
+    "revision": "46f216822d85521ec99c2a3e165dcddb",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-700italic.46f21682.ttf"
+  },
+  {
+    "revision": "5ce0990b59e1ed40e05327f625a24c4a",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-700italic.5ce0990b.woff2"
+  },
+  {
+    "revision": "9e9a574493d4cd2fa62380f376461941",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-700italic.9e9a5744.woff"
+  },
+  {
+    "revision": "a1a6510b0398db3f6bd04abd3c2dfcb8",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-700italic.a1a6510b.svg"
+  },
+  {
+    "revision": "7dfa12661560a957c573a5eff295e819",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-900.7dfa1266.svg"
+  },
+  {
+    "revision": "947e87c53b5765bfc8982613ccd789e9",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-900.947e87c5.woff2"
+  },
+  {
+    "revision": "a82ff6ac9208656f9a21a65dfacbcae3",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-900.a82ff6ac.woff"
+  },
+  {
+    "revision": "d8f0e90eb7c30f7f7780d6136080fb61",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-900.d8f0e90e.eot"
+  },
+  {
+    "revision": "e7d596b2432d63fedce4e621b130d72f",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-900.e7d596b2.ttf"
+  },
+  {
+    "revision": "4ec3ddf2a321d4e3acd01becb17469ab",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-900italic.4ec3ddf2.ttf"
+  },
+  {
+    "revision": "55aee0a97fb337fd18114aa1260d6fdd",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-900italic.55aee0a9.eot"
+  },
+  {
+    "revision": "73d7130e1dc196d25bb35faf2afbbb43",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-900italic.73d7130e.woff"
+  },
+  {
+    "revision": "aee0b2e51f3dc857edbf75de430f8c00",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-900italic.aee0b2e5.svg"
+  },
+  {
+    "revision": "bd86aaa72d23c91de85390a93aa6bef2",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-900italic.bd86aaa7.woff2"
+  },
+  {
+    "revision": "117e995c97eab30fb92843616018d1f1",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-italic.117e995c.woff2"
+  },
+  {
+    "revision": "4f7b172a91b71d1fe950b0b3b7138b10",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-italic.4f7b172a.svg"
+  },
+  {
+    "revision": "92da8468191381556172a57864393d61",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-italic.92da8468.eot"
+  },
+  {
+    "revision": "e652470b9572b86907b77a97f40dc915",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-italic.e652470b.woff"
+  },
+  {
+    "revision": "fef08ee81637dc737081b78dd41655be",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-italic.fef08ee8.ttf"
+  },
+  {
+    "revision": "056a373d808ada4f12a2d6538e88823c",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-regular.056a373d.eot"
+  },
+  {
+    "revision": "9087e4a6aceecc9b2914823044951a3a",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-regular.9087e4a6.svg"
+  },
+  {
+    "revision": "b4d2c4c39853ee244272c04999b230ba",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-regular.b4d2c4c3.woff2"
+  },
+  {
+    "revision": "b8ee546acd6cc0c49f42ad3d48ef244f",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-regular.b8ee546a.woff"
+  },
+  {
+    "revision": "d1d5e30aa8bb081cad8f409f27861672",
+    "url": "/paperid-testcase/static/media/lato-v16-latin-regular.d1d5e30a.ttf"
+  },
+  {
+    "revision": "2cb8e86cdb90bdbe9891576f2e96b0e8",
+    "url": "/paperid-testcase/static/media/paperlogowhite.2cb8e86c.svg"
+  },
+  {
+    "revision": "a4fd8b5fb6921d62024658a4fc4e32ac",
+    "url": "/paperid-testcase/static/media/search.a4fd8b5f.svg"
+  },
+  {
+    "revision": "017b26c97513428d2ee8091f347d115b",
+    "url": "/paperid-testcase/static/media/users.017b26c9.svg"
+  }
+]);
