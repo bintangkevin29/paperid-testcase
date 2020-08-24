c755826132b69d67c9e136007fd7806f6a66f035
Kevin Samosir <bintangkevin29@gmail.com>
2020-08-24 11:00:02 +0700

    added account card

--- src/pages/DashboardPage/DashboardPage.component.tsx
+++ src/pages/DashboardPage/DashboardPage.component.tsx
@@ -14,8 +14,63 @@ const DashboardPage: React.FC = () => {
       <Row>
         <Col className="dashboardPage__transactionSummary" xs={12} md={6}>
           <span className="dashboardPage__cardHeading">Transaction Summary</span>
-          <div className="dashboardPage__chartContainer">
+          <div className="dashboardPage__card">
             <CustomChart />
+          </div>
+        </Col>
+        <Col className="dashboardPage__transactionSummary" xs={12} md={6}>
+          <span className="dashboardPage__cardHeading">Finance Accounts</span>
+          <div className="dashboardPage__card dashboardPage__card--transparent">
+            <Row>
+              <Col lg={6} md={12}>
+                <div className="dashboardPage__accountCard">
+                  <div className="dashboardPage__accountIcon">S</div>
+                  <div className="dashboardPage__accountDetails">
+                    <div className="dashboardPage__accountDetailsName">
+                      <span>Static Example &ndash; </span>
+                      <span className="dashboardPage__accountDetailsName dashboardPage__accountDetailsName--details">
+                        Bank
+                      </span>
+                    </div>
+                    <span className="dashboardPage__accountDetailsName dashboardPage__accountDetailsName--details">
+                      New Account
+                    </span>
+                  </div>
+                </div>
+              </Col>
+              <Col lg={6} md={12}>
+                <div className="dashboardPage__accountCard">
+                  <div className="dashboardPage__accountIcon">S</div>
+                  <div className="dashboardPage__accountDetails">
+                    <div className="dashboardPage__accountDetailsName">
+                      <span>Static Example &ndash; </span>
+                      <span className="dashboardPage__accountDetailsName dashboardPage__accountDetailsName--details">
+                        Bank
+                      </span>
+                    </div>
+                    <span className="dashboardPage__accountDetailsName dashboardPage__accountDetailsName--details">
+                      New Account
+                    </span>
+                  </div>
+                </div>
+              </Col>
+              <Col lg={6} md={12}>
+                <div className="dashboardPage__accountCard">
+                  <div className="dashboardPage__accountIcon">S</div>
+                  <div className="dashboardPage__accountDetails">
+                    <div className="dashboardPage__accountDetailsName">
+                      <span>Static Example &ndash; </span>
+                      <span className="dashboardPage__accountDetailsName dashboardPage__accountDetailsName--details">
+                        Bank
+                      </span>
+                    </div>
+                    <span className="dashboardPage__accountDetailsName dashboardPage__accountDetailsName--details">
+                      New Account
+                    </span>
+                  </div>
+                </div>
+              </Col>
+            </Row>
           </div>
         </Col>
       </Row>
